cf90e27b22dc8f42efb3f72404994c3ca7e456d5
Ionut Hulub <ionut.hulub@gmail.com>
2015-04-28 16:20:46 +0300

    tests almost finished

--- tempest/scenario/test_live_migration.py
+++ tempest/scenario/test_live_migration.py
@@ -20,6 +20,7 @@ from tempest import config
 from tempest import exceptions
 from tempest.scenario import manager
 from tempest import test
+import testtools
 
 CONF = config.CONF
 
@@ -33,6 +34,8 @@ class TestLiveMigrationScenario(manager.ScenarioTest):
     with data on root and ephemeral disk.
 
     """
+
+    _host_key = 'OS-EXT-SRV-ATTR:host'
 
     def glance_image_create(self):
         img_path = CONF.scenario.img_dir + "/" + CONF.scenario.img_file
@@ -68,16 +71,78 @@ class TestLiveMigrationScenario(manager.ScenarioTest):
 
     def _nova_boot(self):
         create_kwargs = {'key_name': self.keypair['name']}
-        self.server = self.create_server(image=self.image,
-                                         create_kwargs=create_kwargs)
+        self.server = self.create_server(
+            image=self.image, create_kwargs=create_kwargs)
 
-    def test_network_connectivity_during_live_migration(self):
+    def _get_compute_hostnames(self):
+        body = self.admin_hosts_client.list_hosts()
+        return [
+            host_record['host_name']
+            for host_record in body
+            if host_record['service'] == 'compute'
+        ]
+
+    def _get_server_details(self, server_id):
+        return self.admin_servers_client.get_server(server_id)
+
+    def _get_host_for_server(self, server_id):
+        return self._get_server_details(server_id)[self._host_key]
+
+    def _migrate_server_to(self, server_id, dest_host):
+        body = self.admin_servers_client.live_migrate_server(
+            server_id, dest_host,
+            CONF.compute_feature_enabled.block_migration_for_live_migration)
+        return body
+
+    def _get_host_other_than(self, host):
+        for target_host in self._get_compute_hostnames():
+            if host != target_host:
+                return target_host
+
+    def _migration(self):
+        actual_host = self._get_host_for_server(self.server['id'])
+        target_host = self._get_host_other_than(actual_host)
+        self._migrate_server_to(self.server['id'], target_host)
+        self.servers_client.wait_for_server_status(self.server['id'], 'ACTIVE')
+        self.assertEqual(target_host, self._get_host_for_server(self.server['id']))
+
+    def _launch_instance(self):
         self.glance_image_create()
         self._nova_keypair_add()
         self._nova_boot()
 
-    def test_migration_of_vm_with_cinder_volume(self):
-        pass
+    @testtools.skipUnless(False, 'skip')
+    def test_network_connectivity_during_live_migration(self):
+        if len(self._get_compute_hostnames()) < 2:
+            raise self.skipTest(
+                "Less than 2 compute nodes, skipping migration test.")
+        self._launch_instance()
+        floating_ip = self.create_floating_ip(self.server)
+        linux_client = self.get_remote_client(floating_ip['ip'])
+        self._migration()
+        linux_client.exec_command('ls')
 
+    @testtools.skipUnless(False, 'skip')
+    def test_migration_of_vm_with_cinder_volume(self):
+        if len(self._get_compute_hostnames()) < 2:
+            raise self.skipTest(
+                "Less than 2 compute nodes, skipping migration test.")
+        self._launch_instance()
+        self.cinder_create()
+        self.nova_volume_attach()
+        self.addCleanup(self.nova_volume_detach)
+        self._migration()
+        self.cinder_show()
+
+    # @testtools.skipUnless(False, 'skip')
     def test_migration_of_vm_with_data_on_root_and_ephemeral_disk(self):
-        pass
+        if len(self._get_compute_hostnames()) < 2:
+            raise self.skipTest(
+                "Less than 2 compute nodes, skipping migration test.")
+        self._launch_instance()
+        floating_ip = self.create_floating_ip(self.server)
+        linux_client = self.get_remote_client(floating_ip['ip'])
+        linux_client.exec_command('echo "Hello World!" > test_live_migration.txt')
+        self._migration()
+        output = linux_client.exec_command('cat test_live_migration.txt').strip()
+        self.assertEqual('Hello World!', output)
